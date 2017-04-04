ea03766c10453598eac7f69839a367965d66d53d
Michael McRoskey <michael.mcroskey@gmail.com>
2017-04-04 13:35:00 -0400

    Added comments

--- main.c
+++ main.c
@@ -6,49 +6,72 @@ The header files page_table.h and disk.h explain
 how to use the page table and disk interfaces.
 */
 
+// Custom header files
 #include "page_table.h"
 #include "disk.h"
 #include "program.h"
 
+// Standard includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
+// Page fault handler
 void page_fault_handler( struct page_table *pt, int page )
 {
 	printf("page fault on page #%d\n",page);
 	exit(1);
 }
 
+// Main execution
 int main( int argc, char *argv[] )
 {
 	if(argc!=5) {
-		printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
+		printf("use: virtmem <npages> <nframes> <rand|fifo|custom> <sort|scan|focus>\n");
 		return 1;
 	}
-
+	
+	// Process command line arguments
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
+	const char *page_replacement_type = argv[3];
 	const char *program = argv[4];
 
+	// Create virtual disk
 	struct disk *disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
 		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
 		return 1;
 	}
 
-
+	// Initialize page_table
 	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
 	if(!pt) {
 		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
 		return 1;
 	}
 
+	// Create virual and physical memory space
 	char *virtmem = page_table_get_virtmem(pt);
-
 	char *physmem = page_table_get_physmem(pt);
+	
+	// Page replacement type case structure
+	if(!strcmp(page_replacement_type,"rand")) {
+		printf("Selected rand");
 
+	} else if(!strcmp(page_replacement_type,"fifo")) {
+		printf("Selected fifo");
+
+	} else if(!strcmp(page_replacement_type,"custom")) {
+		printf("Selected custom");
+
+	} else {
+		fprintf(stderr,"unknown page replacement type: %s\n",argv[3]);
+		return 1;
+	}
+	
+	// Program case structure
 	if(!strcmp(program,"sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
 
@@ -59,7 +82,7 @@ int main( int argc, char *argv[] )
 		focus_program(virtmem,npages*PAGE_SIZE);
 
 	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
+		fprintf(stderr,"unknown program: %s\n",argv[4]);
 		return 1;
 	}
 
