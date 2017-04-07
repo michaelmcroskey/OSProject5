c7c41c0f690aabe41ee1b9843a378e2c40c443c5
mthomann@nd.edu <mthomann@darrow4.cc.nd.edu>
2017-04-07 14:16:27 -0400

    Edited test scripts

--- automate_test.sh
+++ automate_test.sh
@@ -5,9 +5,10 @@
 
 # Usage
 usage() {
-echo "usage:  automate_test.sh [-cn] [-r algorithm]"
+echo "usage:  automate_test.sh [-can] [-r algorithm]"
 echo "  -c:             run the custom test only and append results to the test_results.csv file"
-echo "  -n:             run the fifo & rand tests only and append results to the test_results.csv file"
+echo "  -n:             run the fifo & rand tests only and print results to the test_results.csv file"
+echo "  -a:             run fifo, rand and custom & print results to the test_results.csv file"
 echo "  -r algorithm:   removes a specific algorithm's results from the test file"
 }
 
@@ -25,10 +26,19 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-while getopts 'cnr:' flag; do
+while getopts 'canr:' flag; do
     case "${flag}" in
         c)
             declare -a PAGE_REPLACEMENTS=("custom")
+            ;;
+        a)
+            declare -a PAGE_REPLACEMENTS=("rand" "fifo" "custom")
+            if [ -f test_results.csv ]
+            then
+                rm test_results.csv
+            fi
+            touch test_results.csv
+            printf "COMMAND, Page Replacement Algorithm, Num Pages, Num Frames, Program, Ratio of Pages to Frames, NUM_FAULTS, DISK_READS, DISK_WRITES \n" >> test_results.csv
             ;;
         n)
             declare -a PAGE_REPLACEMENTS=("rand" "fifo")
@@ -57,15 +67,20 @@ do
     echo "Testing $pr ... "
     for p in "${PROGRAMS[@]}"
     do
+        printf "\t\t Program: \t $p \n "
         for rf in "${RATIO_FACTORS[@]}"
         do
+            printf "\t\t Ratio Factor: \t $rf  \n "
             for pn in "${PAGE_NUMS[@]}"
             do
+                printf "\t\t Page Number: \t $pn \n "
                 fn_float=$(echo "$pn * $rf" | bc)
                 fn_int=${fn_float%.*}
+                printf "\t\t Frame Number: \t $fn_int \n "
                 command="./virtmem $pn $fn_int $pr $p"
                 printf "$command , $pr , $pn , $fn_int , $p , $rf , " >> test_results.csv
                 ./virtmem $pn $fn_int $pr $p | grep "," >> test_results.csv
+                printf "\t\t .........................  \n "
             done
         done
     done
